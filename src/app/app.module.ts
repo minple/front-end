bc5a44a6e6018410db2269728ca5a88ea740105d
pumeli <fminple@gmail.com>
2018-08-14 01:36:06 +0700

    dang lam bai, len cty lam tiep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//add for ng-bootstrap
+//add module for using get, post, put, delete
+import { HttpClientModule } from '@angular/common/http';
+
+//start. add module for using the component of ng-bootstrap
 import {NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+//end. add module for using the component of ng-bootstrap
+
 //start. add components for using ng-bootstrap
 import { NgbdAlertCloseable } from './components/modal-basic/modal-basic';
 //end. add components for using ng-bootstrap
@@ -14,6 +19,9 @@ import { MainComponent } from './main/main.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
@@ -24,13 +32,21 @@ import { HeaderComponent } from './header/header.component';
     FooterComponent,
     HeaderComponent,
     //start. add components for using ng-bootstrap
-    NgbdAlertCloseable
-    //end. add components for using ng-bootstrap
+    NgbdAlertCloseable,
+    //end. declare components for using ng-bootstrap
+    PostsComponent,
+    UsersComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbAlertModule
+    //start. import module component for using ng-bootstrap
+    NgbAlertModule,
+    //end. import module component for using ng-bootstrap
+    //import module for using get, post, put, delete
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
